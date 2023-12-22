c80776a1bbe6b5fa2da2e8c2b938938bc2324e1d
Stephen Deutsch <sedeutsch@gmail.com>
2023-12-22 15:58:14 -0500

    Add shadows

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,14 +1,24 @@
 import { Flex } from '@mantine/core';
 import { Welcome } from '../components/Welcome/Welcome';
+import './Home.page.css';
 
 export function HomePage() {
   return (
     <>
-      <Flex direction="column" gap="sm">
-        <Welcome />
-        <Welcome />
-        <Welcome />
-        <Welcome />
+      <Flex direction="column" mt="xl">
+        <Flex justify="center" align="center" h={256} gap="xl">
+          <Flex className="shadowBox shadowXL">XL</Flex>
+          <Flex className="shadowBox shadowLG">LG</Flex>
+          <Flex className="shadowBox shadowMD">MD</Flex>
+          <Flex className="shadowBox shadowSM">SM</Flex>
+          <Flex className="shadowBox shadowXS">XS</Flex>
+        </Flex>
+        {/* <Flex direction="column" gap="sm">
+          <Welcome />
+          <Welcome />
+          <Welcome />
+          <Welcome />
+        </Flex> */}
       </Flex>
     </>
   );
