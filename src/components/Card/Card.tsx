b59c1086cd3b7252af524aaa69f3af48ecb24978
Stephen Deutsch <sedeutsch@gmail.com>
2024-01-22 12:32:22 -0500

    Style select component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,25 +1,44 @@
-import { Card as MyCard, Group, Text, Title, Select, Button } from '@mantine/core';
+import { Card as MyCard, Title, Flex, Select, Button } from '@mantine/core';
 import classes from './Card.module.css';
 
 export default function Card() {
   return (
     <MyCard padding="xl" radius="md" w={420} className={classes.card}>
-      <Group gap="sm">
-        <Title order={4}>Norway Fjord Adventures</Title>
-        <Text size="sm">
-          With Fjord Tours you can explore more of the magical fjord landscapes with tours and
-          activities on and around the fjords of Norway
-        </Text>
-        <Select
-          label="Your favorite library"
-          placeholder="Pick value"
-          data={['React', 'Angular', 'Vue', 'Svelte']}
-        />
+      <Flex direction="column" gap="lg">
+        <Title order={2}>Select</Title>
+        <Flex direction="column" gap="lg">
+          <Select
+            size="md"
+            radius="md"
+            description="Input description"
+            label="Medium"
+            placeholder="Pick value"
+            data={['React', 'Angular', 'Vue', 'Svelte']}
+          />
+
+          <Select
+            size="sm"
+            radius="md"
+            description="Input description"
+            label="Small"
+            placeholder="Pick value"
+            data={['React', 'Angular', 'Vue', 'Svelte']}
+          />
+
+          <Select
+            size="xs"
+            radius="md"
+            description="Input description"
+            label="Xtra Small"
+            placeholder="Pick value"
+            data={['React', 'Angular', 'Vue', 'Svelte']}
+          />
+        </Flex>
 
         <Button color="blue" fullWidth mt="md" radius="md">
           Learn More
         </Button>
-      </Group>
+      </Flex>
     </MyCard>
   );
 }
